9497ef21961aa7a196375e1fbc29996eb3325bce
Glad <8951010+gburboz@users.noreply.github.com>
2018-08-11 00:09:22 -0400

    Add HomeController

---
+++ Step-01-InitialSpringSecurityApp/src/main/java/com/glad/oauth/config/web/HomeController.java
@@ -0,0 +1,16 @@
+package com.glad.oauth.config.web;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public Authentication index(Authentication authentication) {
+        // TODO WARNING! Do not do this as authentication may contain sensitive information
+        return authentication;
+    }
+
+}
