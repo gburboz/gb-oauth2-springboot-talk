81d811fd50c40359727965aae64f8305b9b87195
Glad B <8951010+gburboz@users.noreply.github.com>
2018-08-11 20:15:47 -0400

    Add Step-03-OAuthCustomProvider

---
+++ Step-03-OAuthCustomProviderLogin/src/main/java/com/glad/oauth/config/WebSecurity.java
@@ -0,0 +1,37 @@
+package com.glad.oauth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        // Apply this to all requests
+        http.antMatcher("/**")
+                .authorizeRequests()
+                    // First make sure these URLs are publicly accessible
+                    .antMatchers(
+                            "/",
+                            "/index.html"
+                    ).permitAll()
+                    // All other requests should need an authenticated user
+                    .anyRequest().authenticated()
+                .and()
+                // Logout config
+                .logout()
+                    // Workaround to support logout with both GET and POST (CSRF)
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    // On successful logout go to home URL
+                    .logoutSuccessUrl("/index.html")
+                .and()
+                // Add built-in spring security oauth login page
+                .oauth2Login();
+        // @formatter:on
+    }
+
+}
